c0a7c1d60cb0ed0032863e3312c94d4128afb3fe
Arun Prabhu <arun@Aruns-MacBook-Pro.local>
2026-04-16 20:06:45 -0400

    fix: profile changes - 4

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -19,7 +19,7 @@ export default function AuthorLayout({ children, content }: Props) {
             About
           </h1>
         </div>
-        <div className="pt-8 pb-8 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0 items-start space-y-8">
+        <div className="items-start space-y-8 pt-8 pb-8 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           {/* Sidebar - Profile Card */}
           <div className="flex flex-col items-center space-y-4">
             {avatar && (
